ef505516fc87b73979091e8093acc8946d7ef2db
Ido Ilouz <ilouzido@gmail.com>
2025-05-14 22:43:26 +0300

    add `User` and `Dev tools` + minor fixes

--- install.sh
+++ install.sh
@@ -21,24 +21,44 @@ set -e
 
 # only run with root permission
 if [ "$EUID" -ne 0 ]; then Err "Install script must run with root privileges!"
-    exit 1
-fi
+    exit 1 fi
 
 #==============================
 # Variables
 #==============================
+# some fields will contain sensitive information, dont forget to hide these fields (marked with `# sensitive`)
 # NOTE: 0: Ignore | 1: Apply
-ZONE=""
+USER_NAME=""
+USER_PASS=""  # sensitive
+USER_HOME="/home/$USER_NAME"
 HOSTNAME="MooseBox"
-
-NM_RANDOMIZE_MAC=0
+ZONE=""
 
 USE_WIFI=0
 WIFI_SSID=""
-WIFI_PASS=""  # dont forget to hide this after the install :)
+WIFI_PASS=""  # sensitive
+
+NM_RANDOMIZE_MAC=0
+
+ENABLE_NTP=1
+ENABLE_DOCKER=0
 
 PACKAGES=$(echo "
+zsh
+vim
+neovim
+ntp
 
+# Dev tools
+docker
+docker-compose
+python
+npm
+
+# Font support
+noto-fonts-emoji
+ttf-jetbrains-mono-nerd
+noto-fonts-cjk
 " | sed 's/\n/ /' | sed 's/,//g' | sed 's/#.*$//')
 
 #==============================
@@ -46,24 +66,34 @@ PACKAGES=$(echo "
 #==============================
 # logging
 Err() {
-  echo -e "[\033[0;31mERROR\033[0m $1"
+    echo -e "[\033[0;31mERROR\033[0m $1"
 }
 
 Ok() {
-  echo -e "[\033[0;32mOK   \033[0m $1"
+    echo -e "[\033[0;32mOK   \033[0m $1"
 }
 
 Debug() {
-  echo -e "[\033[0;33mDEBUG\033[0m $1"
+    echo -e "[\033[0;33mDEBUG\033[0m $1"
 }
 
 Info() {
-  echo -e "[\033[0;34mINFO \033[0m $1"
+    echo -e "[\033[0;34mINFO \033[0m $1"
 }
 
 # additional util
 uncomment() {
-  sed -i "s|^\s*#\s*\($2\s*\)|\1|" "$1"
+    sed -i "/^[[:space:]]*#[[:space:]]*$2$/s/^[[:space:]]*#[[:space:]]*//" "$1"
+}
+
+exec_in_user() {
+    sudo -i -u $USER_NAME -- $@
+}
+
+exec_script_in_user() {
+    chown $USER_NAME:$USER_NAME $1
+    chmod a+x $1
+    exec_in_user $@
 }
 
 
@@ -129,7 +159,7 @@ echo "$HOSTNAME" > /etc/hostname
 # Package manager
 #==============================
 Info "Generate updated mirrorlist"
-pacman -Sy reflector
+pacman -Sy --noconfirm reflector
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 reflector --country "Israel" --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 tee -a /etc/pacman.d/mirrorlist <<EOF
@@ -176,12 +206,11 @@ rm -rf /tmp/yay
 Info "Synchronize databases"
 pacman -Syu --noconfirm
 
+Info "Installing required system packages"
+yay -S --noconfirm $PACKAGES
+
 Ok "package manager config done!"
 
-#==============================
-# Packages!
-#==============================
-yay -S
 #==============================
 # Locale
 #==============================
@@ -190,14 +219,69 @@ uncomment /etc/locale.gen "en_US USI-8559-1"
 locale-gen
 
 Info "Download my fav nerdfont :3"
+FONT_DOWNLOAD_LINK="https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/BigBlueTerminal.zip"
 mkdir -p /usr/local/share/font
 mkdir -p /tmp/font
-wget -o /tmp/font/FONT.zip "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/BigBlueTerminal.zip"
+ wget -o /tmp/font/FONT.zip $FONT_DOWNLOAD_LINK
 unzip /tmp/font/FONT.zip
 mv /tmp/font/BigBlueTermPlusNerdFontMono-Regular.ttf /usr/local/share/font
 rm -rf /tmp/font  # cleanup
+fc-cache -fv
 
 #==============================
 # Clock
 #==============================
-hwclock
+Info "Timezone"
+if [[ -f "/usr/share/zoneinfo/$ZONE" ]]; then
+    timedatectl set-timezone $ZONE
+else
+    Err "Zone \"$ZONE\" does not exist"
+
+Info "NTP client setup"
+hwclock --systohc
+[[ $ENABLE_NTP -eq 1 ]] && systemctl enable --now ntpdate
+
+#==============================
+# User
+#==============================
+Info "Create user"
+useradd -mG wheel,audio $USER_NAME
+passwd $USER_NAME <<EOL
+$USER_PASS
+$USER_PASS
+EOL
+
+Info "Configure user shell"
+sudo chsh $USER_NAME --shell /usr/bin/zsh
+curl --proto '=https' --tlsv1.2 -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh > /tmp/oh-my-zsh.sh
+exec_script_in_user /tmp/oh-my-zsh.sh --skip-chsh
+
+tee $USER_HOME <<EOF
+export ZSH="$HOME/.config/oh-my-zsh"
+export PATH="$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH"
+export EDITOR="nvim"
+export XDG_CONFIG_HOME="$HOME/.config"
+
+alias nv="nvim"
+alias gs="git status"
+alias pyenv="souce .venv/bin/activate"
+
+ZSH_THEME="nicoulaj"
+plugins=(git)
+source $ZSH/oh-my-zsh.sh
+EOF
+
+Info "Give user sudo access"
+uncomment /etc/sudoers "wheel ALL=(ALL:ALL) ALL"
+
+#==============================
+# Dev tools
+#==============================
+Info "Rust setup"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > /tmp/rustup.sh
+exec_script_in_user /tmp/rustup.sh
+
+Info "Docker setup"
+usermod -aG docker $USER_NAME
+[[ $ENABLE_DOCKER -eq 1 ]] && sudo systemctl enable --now docker
+
